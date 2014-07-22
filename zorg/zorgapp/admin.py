74152ada254062ade2fb5620d582110c880cf807
shahar-dav <davidson.shahar@gmail.com>
2014-07-22 21:27:31 +0300

    change admin.py for better management of db

--- zorg/zorgapp/admin.py
+++ zorg/zorgapp/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 from zorgapp.models import User,Battle,Topic
 
 admin.site.register(User)
-admin.site.register(Battle)
-admin.site.register(Topic)
+
+
+
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ['name', 'views', 'hits']
+	
+class BattleAdmin(admin.ModelAdmin):
+    list_display = ['winning_topic_name', 'losing_topic_name', 'user_name']
+		
+    def winning_topic_name(self, instance):
+        return instance.winning_topic.name
+		
+    def losing_topic_name(self, instance):
+        return instance.losing_topic.name
+		
+    def user_name(self, instance):
+        return instance.user.user_id
+
+	
+	
+	
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Battle, BattleAdmin)
