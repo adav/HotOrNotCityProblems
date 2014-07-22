8c7730f92b8daf3fc0686bbdf6f22ea6b2d3f918
Greg Schechter <gregthebusker@gmail.com>
2014-07-22 06:24:22 -0700

    Cards
    Cards and switching

--- zorg/zorg/js.js
+++ zorg/zorg/js.js
@@ -1,7 +1,8 @@
 var STATES = {
   init: 0,
   leftWin: 1,
-  rightWin: 2
+  rightWin: 2,
+  loading: 3
 };
 
 var STATE_CLASSES = [];
@@ -22,29 +23,72 @@ $('body').delegate('.worry', 'click', function(e) {
   setState(state);
 });
 
-var createWorryNode = function(data) {
-  var node = $('<a/>', {
-    href: '#',
-    class: 'worry-thumbnail thumbnail'
-  }).append($('<img/>', {
-    class: 'worry-image',
-    style: 'backgroundImage: "url(' + data.url + ')"'
-  }));
-  return node;
-};
-
-var setWorry = function(node, data) {
-  node.empty().append(createWorryNode(data));
-};
-
-var setRight = function(data) {
-  setWorry($('#right'), data);
-};
-var setLeft = function(data) {
-  setWorry($('#left'), data);
+var createWorryNode = function(id, data) {
+  return $('<div/>', {
+    id: id,
+    class: 'worry col-xs-6',
+    style: 'background-image: url(' + data.url + ')'
+  }).append($('<h1/>', { class: "v-center" }).html(data.title));
 };
 
 var receiveBattle = function(data) {
-  setLeft(leftData);
-  setRigth(rightData);
+  showBattleCard(data);
+};
+
+var createCard = function(content, className) {
+  return $('<div/>', {
+    class: 'card ' + className
+  }).append(content);
+};
+
+var createBattleCard = function(data) {
+  var node = createCard();
+  var left = createWorryNode('left', data.left);
+  node.append(left);
+  var right = createWorryNode('right', data.right);
+  node.append(right);
+  return node;
+};
+
+var showInitCard = function() {
+  var node = createCard(
+    $('<h1/>', { class: "v-center" }).html('Zorg'),
+    'init-card');
+  showCard(node);
+};
+
+var showLoadingCard = function() {
+  var node = createCard(
+    $('<h1/>', { class: "v-center" }).html('Loading'),
+    'loading-card');
+  showCard(node);
+};
+
+var showBattleCard = function(data) {
+  var card = createBattleCard(data);
+  showCard(card);
+};
+
+var showCard = function(card) {
+  var cards = $('.card');
+  cards.addClass('leaving');
+  $('body').append(card);
+  cards.addClass('leave');
+};
+
+var sendResults = function(data) {
+};
+
+var fetchBattle = function() {
+};
+
+var sampleData = {
+  left: {
+    title: 'Botfly',
+    url: "http://www.wired.com/images_blogs/wiredscience/2013/10/human-bot-fly-3rd-instar-Buss.jpg"
+  },
+  right: {
+    title: 'Tornado',
+    url: "http://tornado-facts.com/wp-content/uploads/2009/07/lighting-and-tornado-storm.jpg"
+  }
 };
