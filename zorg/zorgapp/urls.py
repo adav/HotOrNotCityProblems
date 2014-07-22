6940af3543acb106a62b13719060c0376dbcbdad
Anthony Arnold <anthony.arnold@uqconnect.edu.au>
2014-07-22 20:53:39 +0300

    Serving front-end from Django

--- zorg/zorgapp/urls.py
+++ zorg/zorgapp/urls.py
@@ -3,8 +3,9 @@ from zorgapp import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('',
-	url(r'^battle', views.BattleView.as_view(), name='battle'),
-	url(r'^topic', views.TopicView.as_view(), name='topic'),
+	url(r'^battle/$', views.BattleView.as_view(), name='battle'),
+	url(r'^topic/', views.TopicView.as_view(), name='topic'),
+    url(r'^$', views.index, name='index'),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- zorg/zorgapp/views.py
+++ zorg/zorgapp/views.py
@@ -3,16 +3,36 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from zorgapp.models import Battle,Topic
 from zorgapp.serializers import BattleSerializer, TopicSerializer
-from rest_framework import mixins, generics
+from rest_framework import mixins, generics, status
+from django.template import RequestContext, loader
+from django.http import HttpResponse
+
+def get_topic_name(text):
+    return text
+    
+def index(request):
+    template = loader.get_template('zorgapp/index.html')
+    return HttpResponse(template.render(RequestContext(request, {})))
 
 class TopicView(APIView):
     def get(self, request):
-
-        topics = Topic.objects.order_by('?')[0:2]
-        
+        #TODO filter out battles the user has done before
+        topics = Topic.objects.order_by('?')[0:20]
         serializer = TopicSerializer(topics, many=True)
         return Response(serializer.data)
-
+        
+    def post(self, request):
+        name = get_topic_name(request.DATA['text'])
+        
+        #TODO Fetch Image URL for name
+        topic = Topic(name=name,hits=0,views=0,img_url='')
+        
+        #TODO Have duplicates return gracefully (hits++, views++ or something)
+        topic.save()
+        
+        serializer = TopicSerializer(topic)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
 class BattleView(mixins.CreateModelMixin,
                  generics.GenericAPIView):
 
