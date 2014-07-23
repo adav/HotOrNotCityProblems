b791840571765796a3c09cf667fb08f7a1d26eeb
Anthony Arnold <anthony.arnold@uqconnect.edu.au>
2014-07-23 11:28:50 +0300

    CSRF and Flickr

--- zorg/zorgapp/urls.py
+++ zorg/zorgapp/urls.py
@@ -5,6 +5,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = patterns('',
 	url(r'^battle/$', views.BattleView.as_view(), name='battle'),
 	url(r'^topic/', views.TopicView.as_view(), name='topic'),
+    url(r'^top/', views.TopView.as_view(), name='top'),
     url(r'^$', views.index, name='index'),
 )
 
