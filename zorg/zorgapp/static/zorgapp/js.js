2ae9b9aa551eae45194a0bcb2ef2d2b9c1cd62aa
Greg Schechter <gregthebusker@gmail.com>
2014-07-22 23:51:06 -0700

    More Analize

--- zorg/zorgapp/static/zorgapp/js.js
+++ zorg/zorgapp/static/zorgapp/js.js
@@ -284,9 +284,134 @@ var showAnalytics = function() {
   showCard(node);
 };
 
+var createRank = function(topic, i) {
+  var div = $('<div/>', {
+    class: "worry-text"
+  });
+  
+  div.append($('<div/>', {
+    class: 'rank-number'
+  }).html((i + 1) + '.'));
+  
+  div.append($('<h1/>').html(topic.name));
+  
+  return $('<li/>', {
+    class: 'rank',
+    style: 'background-image: url(' + topic.img_url + ')'
+  }).append(div);
+};
+
+var showRanking = function(data) {
+  var node = createCard();
+  node.append($('<h1/>', {
+    class: 'ranking-title'
+  }).html(data.title));
+  
+  var ol = $('<ol/>', {
+    class: 'rankings'
+  });
+  for (var i = 0; i < data.topics.length; i++) {
+    var rank = createRank(data.topics[i], i);
+    ol.append(rank);
+  }
+  node.append(ol);
+  showCard(node);
+};
+
 (function init() {
   showInitCard();
   navigator.geolocation.getCurrentPosition(getUserLocation);
   
   $.Topic('addBattles').subscribe(hasMoreBattles);
 })();
+
+var sampleData = {
+  title: 'Problems for the Emerald City',
+  topics: [
+    {
+        "id": 10, 
+        "name": "Corrupt Police", 
+        "img_url": "http://d1pvsnudg6kebv.cloudfront.net/wp-content/uploads/2012/05/Uttarakhand-Police.jpg"
+    }, 
+    {
+        "id": 6, 
+        "name": "Taxes", 
+        "img_url": "http://2.bp.blogspot.com/-E4WtTDSHh_w/TjdwWK-SifI/AAAAAAAAFlM/N56N6UuGFdA/s1600/smoking+uncle_sam_taxes.jpg"
+    }, 
+    {
+        "id": 13, 
+        "name": "Bureaucracy", 
+        "img_url": "http://englishmaninmarseille.files.wordpress.com/2011/12/mp9003993501-1024x1024.jpg"
+    }, 
+    {
+        "id": 5, 
+        "name": "Traffic jams", 
+        "img_url": "http://s1.cdn.autoevolution.com/images/news/how-to-avoid-traffic-jams-35319_2.jpg"
+    }, 
+    {
+        "id": 1, 
+        "name": "Air pollution", 
+        "img_url": "http://www.drsoram.com/wp-content/uploads/air%20pollution%202.jpg"
+    }, 
+    {
+        "id": 17, 
+        "name": "Stolen Bike", 
+        "img_url": ""
+    }, 
+    {
+        "id": 14, 
+        "name": "Drugs", 
+        "img_url": "http://www.m-n-d.co.za/wp-content/uploads/2014/05/nodrugs.png"
+    }, 
+    {
+        "id": 2, 
+        "name": "Dog's poop", 
+        "img_url": "http://www.the3dstudio.com/download_image.ashx?size=large&mode=product&file_guid=1dfac141-2649-4100-a1fb-d770be85a358"
+    }, 
+    {
+        "id": 3, 
+        "name": "Parking", 
+        "img_url": "http://i.dailymail.co.uk/i/pix/2013/04/11/article-2307768-193E7FA1000005DC-662_634x601.jpg"
+    }, 
+    {
+        "id": 7, 
+        "name": "Noise", 
+        "img_url": "http://www.alpinehearingprotection.com/files/2413/7483/4485/earplugs-noise-large.jpg"
+    }, 
+    {
+        "id": 16, 
+        "name": "example", 
+        "img_url": ""
+    }, 
+    {
+        "id": 15, 
+        "name": "Smoking in public places", 
+        "img_url": "http://maneleen.files.wordpress.com/2010/12/no_smoking_law.jpg"
+    }, 
+    {
+        "id": 11, 
+        "name": "Garbage", 
+        "img_url": "http://img.timeinc.net/time/photoessays/2008/naples_garbage/garbage_naples_01.jpg"
+    }, 
+    {
+        "id": 8, 
+        "name": "Beer's Prices", 
+        "img_url": "http://beerbeer.org/wp-content/uploads/2010/10/try-beer-price-01.jpg"
+    }, 
+    {
+        "id": 4, 
+        "name": "Mugging", 
+        "img_url": "https://c2.staticflickr.com/8/7014/6795642413_2849a7df9c_z.jpg"
+    }, 
+    {
+        "id": 9, 
+        "name": "Drunks punks", 
+        "img_url": "http://images.mirror.co.uk/upl/m4/oct2011/6/0/image-10-for-editorial-pics-11-10-2011-gallery-609101548.jpg"
+    }, 
+    {
+        "id": 12, 
+        "name": "Aids", 
+        "img_url": "http://medimoon.com/wp-content/uploads/2012/07/aids-1.jpg"
+    }
+]
+};
