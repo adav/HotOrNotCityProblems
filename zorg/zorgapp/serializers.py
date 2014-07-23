0ff365ed4130301126c1f1151132fb47b4a4dec8
Andrew Davidson <adav@me.com>
2014-07-23 12:52:58 +0300

    S updated serialiser

--- zorg/zorgapp/serializers.py
+++ zorg/zorgapp/serializers.py
@@ -10,4 +10,4 @@ class BattleSerializer(serializers.ModelSerializer):
 class TopicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Topic
-        fields = ('id', 'name', 'img_url')
+        fields = ('id', 'name', 'img_url', 'hits','views')
